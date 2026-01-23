9878a3ef7b5cd2fcedd16ca06ac679201e5ff4c9
patelaryan5636 <patelaryan5636@gmail.com>
2026-01-23 21:00:09 +0530

    Add admin and user authentication middleware
    Introduced adminAuth and userAuth middleware for route protection. Updated app.js to use these middlewares for /admin and /user routes, and added basic login and data endpoints for both admin and user. Also added a global error handler.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 
+const {adminAuth, userAuth} = require("../middlewares/adminAuth");
 const app = express();
 
-app.use("/test",(req,res)=>{
-  res.send("hello from aryan patel");
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).send("Cannot be reached out");
 });
+
+app.get("/user/login",(req,res)=>{
+  res.send("login successfully");
+})
+
+app.use("/admin",adminAuth);
+
+app.use("/user",userAuth);
+
+app.get("/admin/getdata",function(req,res){
+    res.send("data successfully send ");
+  });
+
+app.get("/user/userdata",function(req,res){
+  res.send("data successfuully send to the user");
+})
 
 app.listen(7777,()=>{
   console.log("connect ");
