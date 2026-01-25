b62647a003b6c3a2044db0c0361cdc8add53bb65
patelaryan5636 <patelaryan5636@gmail.com>
2026-01-25 20:22:47 +0530

    Add validator and enhance User model validation
    Introduced the 'validator' package for email validation and updated the User schema to enforce required fields and max lengths. Improved error handling and validation in API routes, including more informative error responses and stricter update logic.

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,23 +1,34 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    trim: true,
+    maxLength: 15,
   },
   lastName: {
     type: String,
+    required: true,
+    maxLength: 20,
   },
   email: {
     type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email id is not valid");
+      }
+    },
   },
   gender: {
     type: String,
   },
-  age:{
+  age: {
     type: Number,
-  }
+  },
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
--- src/app.js
+++ src/app.js
@@ -10,12 +10,14 @@ const app = express();
 
 app.use(express.json());
 
+// middleware for all route error handle
 app.use((err, req, res, next) => {
   console.error(err.stack);
 
   res.status(500).send("Cannot be reached out");
 });
 
+// get user via email id
 app.get("/getuser", async(req,res)=>{
   const getUser = await User.find({email: req.body.email});
 
@@ -26,24 +28,26 @@ app.get("/getuser", async(req,res)=>{
     res.status(400).send("user not found");
   }
   }catch(error){
-    res.status(500).send("bad Request");
+    res.status(500).send("bad Request" + error);
   }
 });
 
+// feed api
 app.get("/feed",async(req,res)=>{
-    const alluser = await User.find({});
-
-    try{
+  
+  try{
+      const alluser = await User.find({});
       if(alluser.length > 0){
         res.send(alluser);
       }else{
         res.status(400),send("users not in database");
       }
     }catch(error){
-      res.status(500).send("bad Request");
+      res.status(500).send("bad Request" + error);
     }
 })
 
+// delete by email id
 app.delete("/user",async(req,res)=>{
   const userid = await User.findOneAndDelete({ email: req.body.email });
 
@@ -54,10 +58,11 @@ app.delete("/user",async(req,res)=>{
       res.status(401).send("email id not found");
     }
   }catch(error){
-    res.status(500).send("bad request");
+    res.status(500).send("bad request" + error);
   }
 })
 
+// signup
 app.post("/signup",async(req,res)=>{
 
   const user = new User(req.body);
@@ -66,27 +71,39 @@ app.post("/signup",async(req,res)=>{
     await user.save();
     res.send("data pushed successfully");
   }catch(error){
-    res.status(400).send("bad request");
+    res.status(400).send("bad request" + error);
   }
 })
 
+// update the user data
 app.patch("/user",async(req,res)=>{
 
-   const { userId, ...data } = req.body; 
+   const { userId, firstName, lastName} = req.body;
+  
+   const UpdateData = {};
+   if(firstName) UpdateData.firstName = firstName;
+   if(lastName) UpdateData.lastName = lastName;
+
+   console.log(Object.keys(UpdateData));
+
+   if (Object.keys(UpdateData).length === 0) {
+     return res.status(400).send("Nothing to update");
+   }
 
    try{
-     const updateUser = await User.findByIdAndUpdate(userId,data);
+     const updateUser = await User.findByIdAndUpdate(userId,UpdateData,{ new: true, runValidators: true });
      if(updateUser){
       res.send("updated succesfully");
     }else{
       res.status(401).send("id not found");
     }
   }catch(error){
-    res.status(500).send("bad request");
+    res.status(500).send("bad request" + error);
   }
 
 });
 
+// connect to the database
 connectDb()
   .then(function () {
     console.log("connection was successfullt established");
